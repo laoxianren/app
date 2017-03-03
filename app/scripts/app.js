31599a60f27d3d8df80f16a3186da494f4f73620
unknown <>
2017-03-03 09:15:56 +0800

    laozheng

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name myappApp
+ * @description
+ * # myappApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('myappApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: '404.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
